2694a8e77beecbf02f207f00aaf865042fa4eeb3
Heikki Mustonen <hmustone18@student.oulu.fi>
2020-05-19 16:52:16 +0300

    Added router and three pages as starting point.

--- src/Components/MainWindow.js
+++ src/Components/MainWindow.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react'
-import StampContainer from './StampContainer'
+//import StampContainer from './StampContainer'
+import {BrowserRouter as Router, Route,Link} from 'react-router-dom'
+import StampListContainer from './StampListContainer'
+import AboutContainer from './AboutContainer'
+import SummaryContainer from './SummaryContainer'
 
 export default class MainWindow extends Component {
+    
+  
+    
     render() {
+
+        var list = () => <StampListContainer stampCollection={this.props.stampCollection}/>
+        var about = () => <AboutContainer />
+        var summary = () => <SummaryContainer />
         return (
             <div className="col MainWindow">
-                <p>this is the main window.</p>
-                <StampContainer stamp={this.props.stampCollection[0]}/>
-                <StampContainer stamp={this.props.stampCollection[1]}/>
-                <StampContainer stamp={this.props.stampCollection[2]}/>
+                <Router>
+                <div className="row">
+                    <div className="col">
+                        <Link className="RouterLink" to="/">List</Link> |
+                        <Link className="RouterLink" to="/Summary">Summary</Link> |
+                        <Link className="RouterLink" to="/About">About</Link>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <Route exact path="/" component={list} />
+                        <Route path="/about" component={about} />
+                        <Route path="/Summary" component={summary} />
+                    </div>
+                </div>
+                </Router>
+               
             </div>
         )
     }
