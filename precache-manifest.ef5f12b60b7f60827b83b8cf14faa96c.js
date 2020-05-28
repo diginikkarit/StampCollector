2d479d05fa1cf9c36944053a63041081924c6b7d
Heikki Mustonen <hmustone18@student.oulu.fi>
2020-05-28 07:07:18 +0300

    Updates

---
+++ precache-manifest.ef5f12b60b7f60827b83b8cf14faa96c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "712b4469ba7e6e60b35e051b75751f6d",
+    "url": "/StampCollector/index.html"
+  },
+  {
+    "revision": "431ec2028e859e17ef7b",
+    "url": "/StampCollector/static/css/main.643d0dc1.chunk.css"
+  },
+  {
+    "revision": "86e7b109908143903cc1",
+    "url": "/StampCollector/static/js/2.0aeefb8f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/StampCollector/static/js/2.0aeefb8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "431ec2028e859e17ef7b",
+    "url": "/StampCollector/static/js/main.b449580e.chunk.js"
+  },
+  {
+    "revision": "2fe75baa8faf075949ce",
+    "url": "/StampCollector/static/js/runtime-main.63e1138e.js"
+  }
+]);
